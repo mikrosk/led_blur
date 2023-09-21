297c37425fdd595f9b4feab6e722eb7a6fce06bd
Miro Kropacek <miro.kropacek@gmail.com>
2023-09-21 20:13:19 +0200

    tabify

--- source/sound.c
+++ source/sound.c
@@ -1,21 +1,21 @@
 /*
-This file is part of the ledblur/mindlapse demo.
-Copyright (c) 2006 Michael Kargas <optimus6128@yahoo.gr>
-
-This program is free software; you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation; either version 2 of the License, or
-(at your option) any later version.
-
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License along
-with this program; if not, write to the Free Software Foundation, Inc.,
-51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
-*/
+ * This file is part of the ledblur/mindlapse demo.
+ * Copyright (c) 2006 Michael Kargas <optimus6128@yahoo.gr>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
 /* UNIX port by John Tsiombikas <nuclear@siggraph.org> */
 #include <stdio.h>
 #include <stdlib.h>
@@ -26,7 +26,7 @@ with this program; if not, write to the Free Software Foundation, Inc.,
 #include <mint/osbind.h>
 
 #define MOD_FILENAME	"retroatt.mod"
-#define SAMPLE_RATE	24585
+#define SAMPLE_RATE		24585
 
 int play_music = 1;
 
@@ -38,89 +38,89 @@ static size_t bufferSize;	// size of one buffer
 static char* pBuffer;
 
 static void loadBuffer(char* pBuffer, size_t bufferSize) {
-    xmp_play_buffer(c, pBuffer, bufferSize, 0);
+	xmp_play_buffer(c, pBuffer, bufferSize, 0);
 }
 
 void SoundInit(void) {
-    if (!play_music)
-        return;
+	if (!play_music)
+		return;
 
-    c = xmp_create_context();
-    
-    struct xmp_test_info ti;
-    xmp_test_module(MOD_FILENAME, &ti);
+	c = xmp_create_context();
 
-    if (xmp_load_module(c, MOD_FILENAME) != 0) {
-	exit(EXIT_FAILURE);
-    }
-    
-    xmp_start_player(c, SAMPLE_RATE, 0);	// 0: stereo 16bit signed (default)
-    
-    bufferSize = 2048*4;	// 2048/24585 = 83ms
+	struct xmp_test_info ti;
+	xmp_test_module(MOD_FILENAME, &ti);
 
-    pBuffer = (char*)Mxalloc(2 * bufferSize, MX_STRAM);
-    if (pBuffer == NULL) {
-    	exit(EXIT_FAILURE);
-    }
-    pPhysical = pBuffer;
-    pLogical = pBuffer + bufferSize;
+	if (xmp_load_module(c, MOD_FILENAME) != 0) {
+		exit(EXIT_FAILURE);
+	}
 
-    loadBuffer(pPhysical, bufferSize);
-    loadBuffer(pLogical, bufferSize);
-    
-    Sndstatus(SND_RESET);
+	xmp_start_player(c, SAMPLE_RATE, 0);	// 0: stereo 16bit signed (default)
 
-    Devconnect(DMAPLAY, DAC, CLK25M, CLK25K, NO_SHAKE);
+	bufferSize = 2048*4;	// 2048/24585 = 83ms
 
-    Setmode(MODE_STEREO16);
-    Soundcmd(ADDERIN, MATIN);
-    Setbuffer(SR_PLAY, pBuffer, pBuffer + 2*bufferSize);
+	pBuffer = (char*)Mxalloc(2 * bufferSize, MX_STRAM);
+	if (pBuffer == NULL) {
+		exit(EXIT_FAILURE);
+	}
+	pPhysical = pBuffer;
+	pLogical = pBuffer + bufferSize;
+
+	loadBuffer(pPhysical, bufferSize);
+	loadBuffer(pLogical, bufferSize);
+
+	Sndstatus(SND_RESET);
+
+	Devconnect(DMAPLAY, DAC, CLK25M, CLK25K, NO_SHAKE);
+
+	Setmode(MODE_STEREO16);
+	Soundcmd(ADDERIN, MATIN);
+	Setbuffer(SR_PLAY, pBuffer, pBuffer + 2*bufferSize);
 }
 
 void PlaySong(void) {
-    if (!play_music)
-        return;
+	if (!play_music)
+		return;
 
-    Buffoper (SB_PLA_ENA | SB_PLA_RPT);
+	Buffoper (SB_PLA_ENA | SB_PLA_RPT);
 }
 
 void UpdateSong(void) {
-    if (!play_music)
-        return;
+	if (!play_music)
+		return;
 
-    static int loadSampleFlag = 1;
+	static int loadSampleFlag = 1;
 
-    SndBufPtr sPtr;
-    if (Buffptr(&sPtr) != 0) {
-	exit(EXIT_FAILURE);
-    }
-
-    if (loadSampleFlag == 0) {
-	// we play from pPhysical (1st buffer)
-	if (sPtr.play < pLogical)
-	{
-            loadBuffer(pLogical, bufferSize);
-            loadSampleFlag = !loadSampleFlag;
+	SndBufPtr sPtr;
+	if (Buffptr(&sPtr) != 0) {
+		exit(EXIT_FAILURE);
 	}
-    } else {
-        // we play from pLogical (2nd buffer)
-        if (sPtr.play >= pLogical) {
-            loadBuffer(pPhysical, bufferSize);
-            loadSampleFlag = !loadSampleFlag;
-        }
-    }
+
+	if (loadSampleFlag == 0) {
+		// we play from pPhysical (1st buffer)
+		if (sPtr.play < pLogical)
+		{
+			loadBuffer(pLogical, bufferSize);
+			loadSampleFlag = !loadSampleFlag;
+		}
+	} else {
+		// we play from pLogical (2nd buffer)
+		if (sPtr.play >= pLogical) {
+			loadBuffer(pPhysical, bufferSize);
+			loadSampleFlag = !loadSampleFlag;
+		}
+	}
 }
 
 void SoundEnd(void) {
-    if (!play_music)
-        return;
+	if (!play_music)
+		return;
 
-    Buffoper(0x00);
+	Buffoper(0x00);
 
-    xmp_stop_module(c);
-    xmp_end_player(c);
-    xmp_release_module(c);
+	xmp_stop_module(c);
+	xmp_end_player(c);
+	xmp_release_module(c);
 
-    Mfree(pBuffer);
-    pBuffer = NULL;
+	Mfree(pBuffer);
+	pBuffer = NULL;
 }
